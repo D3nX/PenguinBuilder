8bb9b0e3dc236ca3b8dfabd56b22973642af9333
HydroGene <ruddyfantelli@gmail.com>
2022-08-24 14:29:57 +0400

    just a simple commit

--- main.rb
+++ main.rb
@@ -129,7 +129,7 @@ class Game < Omega::RenderWindow
 
     def load
         load_quests_map()
-        Omega.set_state(ConstructionState.new)
+        Omega.set_state(CutScene.new)
     end
    
 end
