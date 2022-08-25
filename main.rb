0860237a68d727948f6520ddf4f67b0364edb0f2
D3nX <d3nx.fr@gmail.com>
2022-08-25 09:11:13 +0400

    Set the entry point as cutscene

--- main.rb
+++ main.rb
@@ -132,7 +132,7 @@ class Game < Omega::RenderWindow
 
     def load
         load_quests_map()
-        Omega.set_state(ConstructionState.new)
+        Omega.set_state(CutScene.new)
     end
    
 end
